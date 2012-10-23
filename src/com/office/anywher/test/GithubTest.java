8e49564aa61ef33ce87dc195b6a2e55e1e95eb91
wangzp <zpwang3@gmail.com>
2012-10-23 14:40:54 +0800

    测试github

---
+++ src/com/office/anywher/test/GithubTest.java
@@ -0,0 +1,15 @@
+package com.office.anywher.test;
+
+/**
+ * @author zp
+ * 
+ * ºÜÃ÷ÏÔ£¬²âÊÔgithubµÄ
+ *
+ */
+public class GithubTest {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("²âÊÔgithub");
+	}
+	
+}
