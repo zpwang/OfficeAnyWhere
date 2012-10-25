6fd454774e57dd84000ae42b8bffb1de18ba0708
wangzp <zpwang3@gmail.com>
2012-10-25 14:53:14 +0800

    加入公文处理的创建表语句

--- src/com/office/anywher/db/DbHelper.java
+++ src/com/office/anywher/db/DbHelper.java
@@ -4,23 +4,31 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.office.anywher.offcial.common.OfficialDBConst;
+
 public class DbHelper extends SQLiteOpenHelper{
-	private static final String DATABASE_NAME = "hecheng_oa.db";  
+	
+	private static final String DATABASE_NAME = "hecheng_oa.db";
 	private static final int DATABASE_VERSION = 2;
-	public DbHelper(Context context) {  
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);  
-    }  
+
+	public DbHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(DBConst.CreateTableSQL.CREATE_H_REMEBER_LOGIN_SQL);
+		db.execSQL(OfficialDBConst.CreateGWTableSQL.CREATE_H_GONGWEN_SQL);
 	}
+
 	@Override
-	public void onUpgrade(SQLiteDatabase db, int paramInt1,
-			int paramInt2) {
+	public void onUpgrade(SQLiteDatabase db, int paramInt1, int paramInt2) {
 		db.execSQL(DBConst.DropTableSQL.DROP_H_REMEBER_LOGIN_SQL);
+		db.execSQL(OfficialDBConst.DropGWTableSQL.DROP_H_REMEBER_LOGIN_SQL);
 		onCreate(db);
-	} 
+	}
 }
+
 /*
 NULL 值为NULL
 INTEGER 值为带符号的整型，根据类别用1，2，3，4，6，8字节存储
